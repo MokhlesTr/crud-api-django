eaeb1117e5d38ccb325687d3a7ce13f791c8f045
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-02-01 16:46:02 +0100

    add view

--- newapp/views.py
+++ newapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .models import Member
 def index (request):
-    return render(request,'index.html')
+    mem=Member.objects.all()
+    return render(request,'index.html',{'mem':mem})
